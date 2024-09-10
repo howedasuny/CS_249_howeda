324cea0910ea970dab9c93b62067a1e8a688d729
Deron Howe <howeda@sunypoly.edu>
2024-09-09 23:32:06 -0400

    assignment 1 done, i hope

---
+++ assign01/src/main/java/edu/howeda/assign01/Greetings.java
@@ -0,0 +1,7 @@
+package edu.howeda.assign01;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Mr. Evrart is helping me find my gun.");
+    }
+}
