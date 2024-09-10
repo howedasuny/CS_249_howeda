a8589412c679edf84691f8b9b17d7cb3a1fec535
Dr. Michael J. Reale <realemj@sunypoly.edu>
2024-09-10 15:52:51 -0400

    Fix

--- profexercises/src/main/java/edu/realemj/exercises03/SimpleOregon.java
+++ profexercises/src/main/java/edu/realemj/exercises03/SimpleOregon.java
@@ -1,7 +1,19 @@
 package edu.realemj.exercises03;
 
+import java.util.*;
+
 public class SimpleOregon {
     public static void main(String [] args) {
         System.out.println("WELCOME!");
+
+        Scanner input = new Scanner(System.in);
+
+        double totalFood = input.nextDouble();
+        int numPeople = input.nextInt();
+        int ration = input.nextInt();
+
+        int totalDays = (int)totalFood/(numPeople*ration);
+
+        System.out.println("TOTAL DAYS: " + totalDays);
     }
 }
