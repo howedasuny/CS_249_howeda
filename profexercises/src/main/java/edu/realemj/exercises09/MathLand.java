418d6222b84925391ccb4910580e4a72623920bf
Dr. Michael J. Reale <realemj@sunypoly.edu>
2024-10-03 15:50:49 -0400

    Matrix example

---
+++ profexercises/src/main/java/edu/realemj/exercises09/MathLand.java
@@ -0,0 +1,12 @@
+package edu.realemj.exercises09;
+
+public class MathLand {
+    public static void main(String [] args) {
+        Matrix A = new Matrix(new double[][] {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        });
+        System.out.println(A);
+    }
+}
--- profexercises/src/main/java/edu/realemj/oregon/OregonTrail.java
+++ profexercises/src/main/java/edu/realemj/oregon/OregonTrail.java
@@ -5,6 +5,12 @@ import edu.realemj.exercises03.Supplies;
 
 public class OregonTrail {
     public static void main(String [] args) {
+        System.out.println("Arg cnt: " + args.length);
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(i + ": " + args[i]);
+        }
+
+
         Scanner input = new Scanner(System.in);
 
         // Set who is in the party
