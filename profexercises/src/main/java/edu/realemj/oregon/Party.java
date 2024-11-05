56110576d1bf9ff8b02a21fc98e93320e4a0e038
Dr. Michael J. Reale <realemj@sunypoly.edu>
2024-11-05 15:50:01 -0500

    Exception handling

--- profexercises/src/main/java/edu/realemj/oregon/Party.java
+++ profexercises/src/main/java/edu/realemj/oregon/Party.java
@@ -3,20 +3,25 @@ package edu.realemj.oregon;
 import java.util.*;
 
 public class Party {
-    private Person [] people;
+    //private Person [] people;
+    // Could be final because reference doesn't change
+    private ArrayList<Person> allPeople = new ArrayList<>();
 
     public Party(String [] names) {
         int cnt = names.length;
-        people = new Person[cnt];
+        //people = new Person[cnt];
         for(int i = 0; i < cnt; i++) {
-            people[i] = new Person(names[i]);
+            //people[i] = new Person(names[i]);
+            allPeople.add(new Person(names[i]));
         }
     }
 
     public int getPartyCnt() {
         int aliveCnt = 0;
-        for(int i = 0; i < people.length; i++) {
-            if(people[i].isAlive()) {
+        //for(int i = 0; i < people.length; i++) {
+        for(int i = 0; i < allPeople.size(); i++) {
+            //if(people[i].isAlive()) {
+            if(allPeople.get(i).isAlive()) {
                 aliveCnt++;
             }
         }
@@ -27,15 +32,19 @@ public class Party {
         //for(int i = 0; i < people.length; i++) {
         //    people[i].kill();
         //}
-        for(Person p : people) {
+        for(Person p : allPeople) { // people) {
             p.kill();
         }
+    }
+
+    public void addPerson(String name) {
+        allPeople.add(new Person(name));
     }
 
     public String toString() {
         String s = "PARTY:\n";
         s += getPartyCnt() + " souls alive.\n";
-        for(var p : people) {
+        for(var p : allPeople) { // people) {
             s += "* " + p + "\n";
         }
         //for(int i = 0; i < people.length; i++) {
